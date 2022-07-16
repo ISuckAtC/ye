9bac02548f479551ec15c0f0408a927878455810
Henrik <theporkchopsxd@gmail.com>
2022-07-16 11:28:14 +0200

    made player and bullet prefab + functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Rigidbody rb;
-    public MeshRenderer mr;
+    public Vector2 turnSpeed;
+    private Rigidbody rb;
+    private MeshRenderer mr;
     public GameObject bullet;
 
+    public GameObject head;
     public GameObject bulletSpawnPoint;
 
     public float bulletSpeed;
@@ -17,19 +19,38 @@ public class PlayerController : MonoBehaviour
     public int maxAmmo;
     private float bulletCooldownTimer;
     private int currentAmmo;
+
+    private bool skipFirstFrame = true;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         mr = GetComponent<MeshRenderer>();
+
+        currentAmmo = maxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (skipFirstFrame)
+        {
+            skipFirstFrame = false;
+        }
+        else
+        {
+            Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+            transform.Rotate(Vector3.up, mouseInput.x * turnSpeed.x * Time.deltaTime);
+            head.transform.Rotate(Vector3.right, -mouseInput.y * turnSpeed.y * Time.deltaTime);
+        }
+
+
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        rb.velocity = new Vector3(input.x * speed, rb.velocity.y, input.y * speed);
+        Vector3 movement = (input.x * transform.right + input.y * transform.forward);
+
+        rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movement.z * speed);
 
         if (bulletCooldownTimer <= 0)
         {
@@ -38,7 +59,7 @@ public class PlayerController : MonoBehaviour
                 bulletCooldownTimer = bulletCooldown;
                 currentAmmo--;
 
-                Vector3 direction = bulletSpawnPoint.transform.position - Camera.main.transform.position;
+                Vector3 direction = bulletSpawnPoint.transform.position - head.transform.position;
 
                 GameObject bulletClone = Instantiate(bullet, bulletSpawnPoint.transform.position, Quaternion.identity);
 
@@ -51,6 +72,11 @@ public class PlayerController : MonoBehaviour
         else
         {
             bulletCooldownTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentAmmo = maxAmmo;
         }
     }
 }
