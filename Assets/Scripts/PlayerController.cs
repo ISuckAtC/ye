3e2a60120b3daa4a506be2a7e1f3c75635f13f0f
Henrik <theporkchopsxd@gmail.com>
2022-07-16 19:27:52 +0200

    ihusdfhiusdf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -88,7 +88,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(health);
 
         if (skipFirstFrame)
         {
