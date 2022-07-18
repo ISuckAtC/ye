969186492c33e657a02ccc689fee237b9109d3c3
Rafael Ribeiro <lurafaribeiro@gmail.com>
2022-07-18 03:18:44 +0100

    Second Level populated

---
+++ Assets/GameManagerSL.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSL : MonoBehaviour
+{
+    private enum Stages { };
+    Stages stages;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
