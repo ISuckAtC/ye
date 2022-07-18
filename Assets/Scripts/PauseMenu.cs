510a93917b5d89de75b4c04191d994db77c1c469
Quick-Bolt <nuno.gnu1@gmail.com>
2022-07-18 15:56:31 +0100

    pause menu wip

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+
+        pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        gameIsPaused = true;
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
